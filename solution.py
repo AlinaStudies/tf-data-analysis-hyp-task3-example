794da87ad3a0090c0bb9b6240f63ccb887efb9f3
AlinaStudies <91690629+AlinaStudies@users.noreply.github.com>
2023-04-13 23:43:23 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,11 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import permutation_test
+from scipy.stats import mannwhitneyu
 
 
 chat_id = 721973830
 
-def statistic(x, y, axis):
-    return np.mean(x, axis=axis) - np.mean(y, axis=axis)
-
 def solution(x: np.array, y: np.array) -> bool:
     alpha = 0.03
-    test = permutation_test((x, y), statistic, vectorized=True, alternative='less')
+    test = mannwhitneyu(x, y, alternative = "less")
     return test.pvalue < alpha
