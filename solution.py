394644b7c823e9f2f6a66cc36df8f154aee003ff
AlinaStudies <91690629+AlinaStudies@users.noreply.github.com>
2023-04-13 23:04:52 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,10 +5,10 @@ from scipy.stats import permutation_test
 
 chat_id = 721973830
 
-def statistic(x, y):
-    return np.mean(x) - np.mean(y)
+def statistic(x, y, axis):
+    return np.mean(x, axis=axis) - np.mean(y, axis=axis)
 
 def solution(x: np.array, y: np.array) -> bool:
     alpha = 0.03
-    _, pval, _ = permutation_test((x, y), statistic, vectorized=True, alternative='less')
-    return pval < alpha
+    test = permutation_test((x, y), statistic, vectorized=True, alternative='less')
+    return test.pvalue < alpha
